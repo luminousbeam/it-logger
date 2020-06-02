c6f9839d58d1a30e21469f790eda3e44c4417032
luminousbeam <jocelyn.jeriah@gmail.com>
2020-06-02 17:03:38 -0400

    Edit & TechModal components

--- src/components/layout/AddBtn.js
+++ src/components/layout/AddBtn.js
@@ -17,7 +17,7 @@ export const AddBtn = () => {
           </a>
         </li>
         <li>
-          <a href='#tech-modal' className='btn-floating red modal-trigger'>
+          <a href='#add-tech-modal' className='btn-floating red modal-trigger'>
             <i className='material-icons'>person_add</i>
           </a>
         </li>
--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-export const AddLogModal = () => {
+export const EditLogModal = () => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
@@ -11,7 +11,7 @@ export const AddLogModal = () => {
       M.toast({ html: 'Please enter a message and tech' });
     } else {
       console.log(message, tech, attention);
-
+      
       //clear fields
       setMessage('');
       setTech('');
@@ -20,7 +20,7 @@ export const AddLogModal = () => {
   };
 
   return (
-    <div id='add-log-modal' className='modal' style={modalStyle}>
+    <div id='edit-log-modal' className='modal' style={modalStyle}>
       <div className='modal-content'>
         <h4>Enter System Log</h4>
         <div className='row'>
@@ -86,4 +86,4 @@ const modalStyle = {
   height: '75%',
 };
 
-export default AddLogModal;
+export default EditLogModal;
